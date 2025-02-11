f670acb8ac73ecfd0c7d350152f8d0d265f5d866
Julian Rüth <julian.rueth@fsfe.org>
2025-02-11 11:29:15 +0100

    Drop build-aux patches
    there's no build-aux directory anymore, so probably they don't ship
    those files.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build-aux
-
 export CFLAGS="-fPIC $CFLAGS"
 export CXXFLAGS="-fPIC $CXXFLAGS"
 
