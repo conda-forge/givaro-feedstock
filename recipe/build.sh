097dc92fc6fa919d0f2a95bc587b8c68e5460b1e
Isuru Fernando <isuruf@gmail.com>
2025-03-27 10:04:30 -0500

    Revert "remove clang 14 workaround"
    This reverts commit 23eb55824c3ec46bc264d1ca56e2a56b08a56b49.

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,3 +33,9 @@ make install
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   make check || (cat tests/test-suite.log; false)
 fi
+
+if [[ "$target_platform" == osx-64 ]]; then
+  sed -i.bak "s/Cflags:/Cflags: -fclang-abi-compat=14/g" $PREFIX/lib/pkgconfig/givaro.pc
+  rm $PREFIX/lib/pkgconfig/givaro.pc.bak
+  cat $PREFIX/lib/pkgconfig/givaro.pc
+fi
